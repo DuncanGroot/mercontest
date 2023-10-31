55157956c8021d359be80916751c0136eb6e23be
Duncan Groot <34777@ma-web.nl>
2023-10-31 14:46:01 +0100

    changed primary file

--- mergeconftest.js
+++ mergeconftest.js
@@ -2,8 +2,11 @@ function conflictTest(){
     console.log("test1")
 }
 function conflictTest2(){
-    let e = 2;
+    let e = 3;
     if (e == 2){
         console.log("e, is in fact 2")
+    }
+    else{
+        console.log("not 2 I guess")
     }
 }
